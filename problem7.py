e16dbbd22862d59d10cb52168daccda6ab57fec8
BartoszThielmann <bartoszthielmann@gmail.com>
2022-08-20 09:01:32 +0200

    commited problem 7

--- problem7.py
+++ problem7.py
@@ -1,2 +1,16 @@
 #! python3
-#what is the 10 001st prime number?
+# What is the 10 001st prime number?
+
+list_of_primes=[]
+number_checked=1
+while len(list_of_primes)<10001:
+    number_checked +=1
+    print('Number checked:' + str(number_checked))
+    pierwsza = True
+    for i in range(2,number_checked-1):
+        if number_checked%i==0:
+            pierwsza = False
+            break
+    if pierwsza == True:
+        list_of_primes.append(number_checked)
+print(list_of_primes[-1])
